6b4d9fd72f30a4058ee373e359cf76d40ddeb1d6
lukasdragon <lukasdragon@live.ca>
2022-08-16 15:28:39 -0700

    Changed ICommand to ReactiveCommands

--- MC_DataHelper/ViewModels/MainWindowViewModel.cs
+++ MC_DataHelper/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Windows.Input;
+using ReactiveUI;
 
 namespace MC_DataHelper.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+   
+
         public bool IsProjectOpen
         {
             get { return true; }
@@ -14,19 +18,19 @@ namespace MC_DataHelper.ViewModels
 
 
         //File menu commands
-        public ICommand NewProjectCommand { get; }
-        public ICommand OpenProjectCommand { get; }
-        public ICommand SaveProjectCommand { get; }
-        public ICommand SaveProjectAsCommand { get; }
-        public ICommand ExitCommand { get; }
+        public ReactiveCommand<Unit, Unit> NewProjectCommand { get; }
+        public ReactiveCommand<Unit, Unit> OpenProjectCommand { get; }
+        public ReactiveCommand<Unit, Unit> SaveProjectCommand { get; }
+        public ReactiveCommand<Unit, Unit> SaveProjectAsCommand { get; }
+        public ReactiveCommand<Unit, Unit> ExitCommand { get; }
 
 
         //Edit menu commands
-        public ICommand UndoCommand { get; }
-        public ICommand RedoCommand { get; }
-        public ICommand CopyCommand { get; }
-        public ICommand PasteCommand { get; }
-        public ICommand BiomeCSVWindowCommand { get; }
+        public ReactiveCommand<Unit, Unit> UndoCommand { get; }
+        public ReactiveCommand<Unit, Unit> RedoCommand { get; }
+        public ReactiveCommand<Unit, Unit> CopyCommand { get; }
+        public ReactiveCommand<Unit, Unit> PasteCommand { get; }
+        public ReactiveCommand<Unit, Unit> BiomeCsvWindowCommand { get; }
 
 
       
