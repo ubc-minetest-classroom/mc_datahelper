ff0baf955e34bc36228887990a64dc7c2a68146e
lukasdragon <lukasdragon@live.ca>
2022-08-16 15:19:33 -0700

    Added command properties in the MainWindowViewModel

--- MC_DataHelper/ViewModels/MainWindowViewModel.cs
+++ MC_DataHelper/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,34 @@
-﻿namespace MC_DataHelper.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MC_DataHelper.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public bool IsProjectOpen
+        {
+            get { return true; }
+        }
+        
+        public ObservableCollection<string> TreeViewItems { get; }
+
+
+        //File menu commands
+        public ICommand NewProjectCommand { get; }
+        public ICommand OpenProjectCommand { get; }
+        public ICommand SaveProjectCommand { get; }
+        public ICommand SaveProjectAsCommand { get; }
+        public ICommand ExitCommand { get; }
+
+
+        //Edit menu commands
+        public ICommand UndoCommand { get; }
+        public ICommand RedoCommand { get; }
+        public ICommand CopyCommand { get; }
+        public ICommand PasteCommand { get; }
+        public ICommand BiomeCSVWindowCommand { get; }
+
+
+      
     }
 }
