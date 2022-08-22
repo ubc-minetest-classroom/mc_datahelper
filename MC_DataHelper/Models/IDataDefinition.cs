8b77db9622c4210373aa93d7b21f8353c8206ca8
lukasdragon <lukasdragon@live.ca>
2022-08-21 18:15:03 -0700

    Added json.net property bindings

--- MC_DataHelper/Models/IDataDefinition.cs
+++ MC_DataHelper/Models/IDataDefinition.cs
@@ -1,7 +1,11 @@
-﻿namespace MC_DataHelper.Models;
+﻿using Newtonsoft.Json;
+
+namespace MC_DataHelper.Models;
 
 public interface IDataDefinition
 {
+    [JsonProperty(propertyName: "name")]
     string Name { get; }
+    [JsonProperty(propertyName: "_jsonType")]
     public string JsonType { get; }
 }
