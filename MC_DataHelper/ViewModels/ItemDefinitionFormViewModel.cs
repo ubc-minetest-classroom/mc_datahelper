d45c6edb555a3839ae38ff9ae1e7415bd0160d09
lukasdragon <lukasdragon@live.ca>
2022-09-02 19:55:13 -0700

    Started defining viewmodel for item definitions

--- MC_DataHelper/ViewModels/ItemDefinitionFormViewModel.cs
+++ MC_DataHelper/ViewModels/ItemDefinitionFormViewModel.cs
@@ -4,6 +4,36 @@ namespace MC_DataHelper.ViewModels;
 
 public class ItemDefinitionFormViewModel : DataDefinitionFormViewModelBase<CraftItemDataDefinition>
 {
+    public string? Description => Data.Description;
+
+    public string? ShortDescription => Data.ShortDescription;
+
+    public string? InventoryImage => Data.InventoryImage;
+
+    public string? WieldImage => Data.WieldImage;
+    public string? WieldOverlay => Data.WieldOverlay;
+    public string? Palette => Data.Palette;
+    public string? Color => Data.Color;
+    public string? WieldScale => Data.WieldScale;
+
+    public string? StackMax
+    {
+        get => Data.StackMax.ToString();
+        set
+        {
+            if (int.TryParse(value, out var number)) Data.StackMax = number;
+        }
+    }
+    
+    public string? Range
+    {
+        get => Data.Range.ToString();
+        set
+        {
+            if (int.TryParse(value, out var number)) Data.Range = number;
+        }
+    }
+
     protected override void UpdateProperties()
     {
         base.UpdateProperties();
